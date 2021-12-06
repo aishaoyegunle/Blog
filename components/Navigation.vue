50a15a3295366ae8294201689aae1844976b1178
Aisha Oyegunle <motunrayoaisha@gmail.com>
2021-12-06 20:43:13 +0300

    add base structure

---
+++ components/Navigation.vue
@@ -0,0 +1,13 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+@import '@/assets/scss/components/_navigation.scss';
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <Navigation />
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+    Navigation: () => import('@/components/Navigation.vue'),
+  },
+}
+</script>
